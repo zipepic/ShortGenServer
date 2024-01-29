05fbdae35950d8c823fff871cb6ed5d224eda947
xzz1p <olezhka.kirilov.97@mail.ru>
2024-01-29 22:46:20 +0300

    Initial commit

---
+++ src/main/java/com/example/shortgenserver/ShortGenServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.shortgenserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortGenServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortGenServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shortgenserver/ShortGenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shortgenserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortGenServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
